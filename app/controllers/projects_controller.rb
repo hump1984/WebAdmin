3326dbfd24c6d6ce230adfd2a021abc80aa64c9a
Thomas Storvik <t_storvik@hotmail.com>
2016-07-16 09:55:19 +0000

    Fixed bug in controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,8 +19,6 @@ class ProjectsController < ApplicationController
   
     def create
         @project = Project.new(project_params)
-        @project.save
-        redirect_to @project
       if @project.save
         redirect_to @project
       else
